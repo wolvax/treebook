601176161844c27964806e65f1653ba3bea94e0b
wolvax <wolvax2@gmail.com>
2012-10-26 02:12:44 -0200

    26/10 - 02:12

--- db/migrate/20121024214157_add_user_id_to_statuses.rb
+++ db/migrate/20121024214157_add_user_id_to_statuses.rb
@@ -1,7 +1,7 @@
 class AddUserIdToStatuses < ActiveRecord::Migration
   def change
-  	add_columm :statuses, :user_id, :integer
+  	add_column :statuses, :user_id, :integer
   	add_index :statuses, :user_id
-  	remove_columm :statuses, :name
+  	remove_column :statuses, :name
   end
 end
